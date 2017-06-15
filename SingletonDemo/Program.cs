a1f79f17a91459f774a3d940a83c7c49896d0ff3
khanhlive <khanhngdinh@outlook.com>
2017-06-15 13:07:44 +0700

    updated 15/06/2017

--- SingletonDemo/Program.cs
+++ SingletonDemo/Program.cs
@@ -10,6 +10,25 @@ namespace SingletonDemo
     {
         static void Main(string[] args)
         {
+            /*
+             * Assuming Singleton is created from employee class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+            /*
+             * Assuming Singleton is created from student class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            /*
+             * Instantiating singleton from a derived class. This violates singleton pattern principles.
+             */
+            Singleton.DerivedSingleton derivedObj = new Singleton.DerivedSingleton();
+            derivedObj.PrintDetails("From Derived");
+            
+            Console.ReadLine();
         }
     }
 }
